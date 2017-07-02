7c1ef52bde16c831ad7662c4dbd2e35c1baca27d
karolskora1993 <karol.skora@wingu.de>
2017-07-02 15:10:43 +0200

    linked_list

---
+++ linked_list.py
@@ -0,0 +1,95 @@
+
+class Node(object):
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList(object):
+
+    def __init__(self):
+        self.head = None
+        self.length = 0
+
+    def insert_at_beginning(self, data):
+        node = Node(data)  
+        if self.head:
+            node.next = self.head
+        self.head = node
+        self.length += 1
+
+    def insert_at_end(self, data):
+        node =  Node(data)
+        if self.head:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = node
+        else:
+            self.head = node
+        self.length += 1
+
+    def remove_first(self):
+        if self.head:
+            self.head = self.head.next
+        else:
+            self.head = None
+        self.length -= 1
+
+    def remove_last(self):
+        current = self.head
+        while current.next.next:
+            current = current.next
+        current.next = None
+        self.length -= 1
+
+    def remove(self, data):
+        current = self.head
+        prev = None
+        while current.next:
+            if current.data == data:
+                if prev and prev.next:
+                    prev.next = current.next
+                else:
+                    self.head = None
+                self.length -= 1
+                return
+            prev = current
+            current = current.next
+        print('item not found')
+
+    def get_length(self):
+        return self.length  
+
+    def traverse(self):
+        current = self.head
+        while current:
+            print(current.data)
+            current = current.next
+
+
+def main():
+    list = LinkedList()
+    list.insert_at_beginning(1)
+    list.insert_at_beginning(0)
+    list.insert_at_end(2)
+    list.insert_at_end(3)
+    list.insert_at_end(4)
+    list.insert_at_end(5)
+    print(list.traverse())
+    list.remove_first()
+    print(list.traverse())
+    list.remove_last()
+    print(list.traverse())
+    list.insert_at_end(44)
+    list.insert_at_end(55)
+    list.insert_at_end(66)
+    list.insert_at_end(77)
+    list.remove(66)
+    print(list.traverse())
+    print(list.get_length())
+
+
+if __name__ == '__main__':
+    main()
